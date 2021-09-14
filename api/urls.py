1960a15b48c018d7a7581eb067339ffe397ec98d
lee kangmin <kangmin7434@naver.com>
2021-09-14 14:48:20 +0900

    test

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,6 @@ from .views import PostView, PostCategoryView, PostDetailView
 urlpatterns= [
     path('auth/',include('rest_framework.urls', namespace='rest_framework')),
     path('posts/', PostView.as_view(), name='post_list'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('posts/<int:pk>/detail', PostDetailView.as_view(), name='post_detail'),
     path('posts/<str:category>/', PostCategoryView.as_view(), name='post_category')
 ]
