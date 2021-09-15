29c8effa55749ee876d68369c92aa831aa1d0648
lee kangmin <kangmin7434@naver.com>
2021-09-15 11:24:18 +0900

    allow host

--- django_api/settings.py
+++ django_api/settings.py
@@ -52,7 +52,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["1iqxc2u93c.execute-api.ap-northeast-2.amazonaws.com","127.0.0.1"]
+ALLOWED_HOSTS = ["1iqxc2u93c.execute-api.ap-northeast-2.amazonaws.com","127.0.0.1","u1fl33oe2b.execute-api.ap-northeast-2.amazonaws.com"]
 
 
 # Application definition
